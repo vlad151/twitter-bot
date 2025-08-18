9205dc395d712aa28c74315366fadd372a449caa
vlad lazar <lazar.vlad151@gmail.com>
2025-08-18 23:26:07 +0200

    update repo shell script

---
+++ update_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/vlad/Desktop/twitter-bot/
+
+# Run the Python script and append the output to cron.log
+# The 2>&1 redirects standard error to standard output
+/usr/bin/python3 script.py >> cron.log 2>&1
+
+# Add the cron.log file to the Git staging area
+git add cron.log
+
+# Commit the changes with a dynamic timestamp
+git commit -m "Cron: Updated log on $(date)"
+
+# Push the changes to the remote repository
+git push origin main
